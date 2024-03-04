356bef07ae1360792202709166d99916191dde16
gaelLara21 <gaellara19@gmail.com>
2024-03-03 23:12:51 -0600

    2_5 Manejo y optimizacion de imagenes

--- src/app/components/home/Description/description.tsx
+++ src/app/components/home/Description/description.tsx
@@ -1,7 +1,15 @@
+import styles from './Description.module.sass'
+import Image from 'next/image'
+
 export const Description = () =>{
     return(
-    <section>
-       <img src="/public/images/description.jpeg" alt="products marketplace"/>
+    <section className={styles.Description}>
+       <Image src="/public/images/description.jpeg" 
+       alt="products marketplace" 
+       width={500} 
+       height={300}
+       />
+
        <h2>Descripción</h2>
        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis reprehenderit et provident aliquid cum perferendis dolorum dicta repellat. Consequatur aperiam praesentium facere adipisci a perferendis vel in nisi odio quasi.</p>
     </section>
