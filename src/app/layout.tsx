dbc96d6a1bb738d0938b826ed89f7eeb21f2f290
gaelLara21 <gaellara19@gmail.com>
2024-03-03 19:50:12 -0600

    09 Creación de arquitectura de Landing Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,20 @@
-"use client"
-
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { Header } from "./components/Header";
+import { Header } from "./components/shared/Header";
+import {Footer} from "./components/shared/Footer"
 
 const inter = Inter({ subsets: ["latin"] });
-
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  console.log('Hola Mundo layout')
   return (
     <html lang="en">
-      <Header/>
       <body className={inter.className}>
+      <Header/>
         {children}
+        <Footer/>
         </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-"use client"
+import { Description } from "./components/home/Description/description";
+import { Hero } from "./components/home/Hero";
+import { MainProducts } from "./components/home/MainProducts";
 
-import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
-  console.log('Hola Mundo')
+
   return (
-    <main className={styles.main}>
-<h1>Hola Mundo</h1>
+    <main>
+      <Hero/>
+      <Description/>
+      <MainProducts/>
     </main>
   );
 }
