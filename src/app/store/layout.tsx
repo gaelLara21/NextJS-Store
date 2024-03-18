eb69f27d0fa75b03d864c17b1ad9144fe325e287
gaelLara21 <gaellara19@gmail.com>
2024-03-18 17:50:25 -0600

    3_5 Route grouping

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -1,8 +1,27 @@
-export default function Layout({children}: {children: React.ReactNode}){
+import { Description } from "../components/home/Description";
+import { Hero } from "../components/home/Hero";
+import { Footer } from "../components/shared/Footer";
+import { Header } from "../components/shared/Header";
+import '/src/app/sass/globals.sass'
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+    weight: ["100","300","500","700"],
+    subsets: ["latin"],
+})
+
+export default function RootLayout({children}: 
+
+    {children: React.ReactNode
+    }){
+
     return(
-        <main>
-            <nav>Navegación de las categorias</nav>
-            {children}
-        </main>
+<html lang="en">
+    <body className={roboto.className}>
+        <Header />
+        {children}
+        <Footer/>
+    </body>
+</html>
     )
 }
