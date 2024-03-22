5fe098d9627568d8132fedebf306f353ea68995b
gaelLara21 <gaellara19@gmail.com>
2024-03-21 23:05:44 -0600

    4_8_filtrando categorias de productos

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -9,11 +9,19 @@ interface CategoryProps{
     searchParams?: string
 }
 
-export default async function Category (props: CategoryProps){
-    const products = await getProducts()
+export default async function Category(props: CategoryProps) {
+    const { categories } = props.params
+    let products = []
     const collections = await getCollections()
-    const{categories} = props.params
-    return(
-        <ProductsWrapper products={products}/>
+    
+    if (categories?.length > 0) {
+      const selectedCollectionId = collections.find((collection: any) => collection.handle === categories[0]).id
+      products = await getCollectionsProducts(selectedCollectionId)
+    }else {
+      products = await getProducts()
+    }
+  
+    return (
+      <ProductsWrapper products={products} />
     )
-}
+  }
--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -6,6 +6,8 @@ export const shopifyUrls ={
         'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`
     },
     collections:{
-        'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/smart_collections.json`
+        'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/smart_collections.json`,
+
+        'products': (id: string) =>`${env.SHOPIFY_HOSTNAME}/admin/api/2024-01/collections/${id}/products.json`
     }
 }
