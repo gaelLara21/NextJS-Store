83285fa6769c2d9aa7d018340f1fee882ac5e104
gaelLara21 <gaellara19@gmail.com>
2024-04-15 23:04:01 -0600

    5_3 proceso de signup con graphql

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,21 @@
 "use server"
 
-export const handleCreateUser = (formData)=>{
-    console.log("handleCreateUser", formData)
+import { GraphQLClientSingleton } from "app/graphql"
+import { createUserMutation } from "app/graphql/mutations/createUserMutation"
+
+export const handleCreateUser = async (formData: FormData)=>{
+    
+    const formDataObject = Object.fromEntries(formData)
+    delete formDataObject["password_confirmation"]
+    const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+    const variables = {
+        input: {
+            ...formDataObject,
+            phone: '+52' + formDataObject.phone
+          }
+        }
+
+    const data = await graphqlClient.request(createUserMutation, variables)
+    console.log(data)
+
 }
